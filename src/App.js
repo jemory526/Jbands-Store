8cb60159451fbef65ecdb208ed42a00c33109acd
Jordan Emory <jemory526@gmail.com>
2021-03-08 16:13:54 -0800

    added checkout w/ bug in country selection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
-import { Products, Navbar, Cart } from './components';
+import { Products, Navbar, Cart, Checkout } from './components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState({});
+    const [order, setOrder] = useState({});
+    const [errorMessage, setErrorMessage] = useState("");
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
@@ -18,15 +20,45 @@ const App = () => {
     }
 
     const handleAddtoCart = async (productId, quantity) => {
-        const response = await commerce.cart.add(productId, quantity);
+        const { cart } = await commerce.cart.add(productId, quantity);
 
-        setCart(response.cart)
+        setCart(cart)
     }
 
     const handleUpdateCartQty = async (productId, quantity) => {
-        const response = await commerce.cart.update(productId, { quantity });
+        const { cart } = await commerce.cart.update(productId, { quantity });
 
-        setCart(response.cart)
+        setCart(cart)
+    }
+
+    const handleRemoveFromCart = async (productId) => {
+        const { cart } = await commerce.cart.remove(productId);
+
+        setCart(cart)
+    }
+
+    const handleEmptyCart = async () => {
+        const { cart } = await commerce.cart.empty();
+
+        setCart(cart)
+    }
+
+    const refreshCart = async () => {
+        const newCart = await commerce.cart.refresh();
+
+        setCart(newCart)
+    }
+
+    const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+        try {
+            const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+            setOrder(incomingOrder);
+            refreshCart()
+        }
+        catch (error) {
+            setErrorMessage(error.data.error.message)
+        }
     }
 
     useEffect(() => {
@@ -34,7 +66,6 @@ const App = () => {
         fetchCart();
     }, []);
 
-    console.log(cart)
 
     return (
         <Router>
@@ -45,7 +76,20 @@ const App = () => {
                         <Products products={products} onAddToCart={handleAddtoCart} />
                     </Route>
                     <Route exact path="/cart">
-                        <Cart cart={cart} />
+                        <Cart
+                            cart={cart}
+                            handleUpdateCartQty={handleUpdateCartQty}
+                            handleRemoveFromCart={handleRemoveFromCart}
+                            handleEmptyCart={handleEmptyCart}
+                        />
+                    </Route>
+                    <Route exact path="/checkout">
+                        <Checkout
+                            cart={cart}
+                            order={order}
+                            onCaptureCheckout={handleCaptureCheckout}
+                            error={errorMessage}
+                        />
                     </Route>
                 </Switch>
             </div>
